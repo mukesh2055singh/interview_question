9f9d521937ca62caffa689c31a1b54b234a78b57
Mukesh Singh <mukesh2055.singh@ibm.com>
2024-06-26 02:35:04 +0530

    fist commit

---
+++ src/main/java/com/ratingrervice/RatingBean.java
@@ -0,0 +1,40 @@
+package com.ratingrervice;
+
+public class RatingBean {
+	private Integer id;
+	private String hotelId;
+	private String userId;
+	private String rating;
+	private String feedback;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getHotelId() {
+		return hotelId;
+	}
+	public void setHotelId(String hotelId) {
+		this.hotelId = hotelId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public String getFeedback() {
+		return feedback;
+	}
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+	
+}
---
+++ src/main/java/com/ratingrervice/RatingController.java
@@ -0,0 +1,25 @@
+package com.ratingrervice;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratings")
+public class RatingController {
+	
+	
+	@GetMapping
+	public String getAllRating(){
+		System.out.println("ratings mapping called");
+		 return "successful";
+	}
+}
